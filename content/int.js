196a2d74b876219fdbd9a76b1b921c762fd2f870
shagrirse <shagrirse@gmail.com>
2019-09-27 02:08:17 +0800

    jquery update

--- content/int.js
+++ content/int.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
     jQuery('#navbar').on('click', function(event) {        
-        jQuery('#navhidden').toggle('show');
+        jQuery('#navhidden').fadeToggle('fast');
     });
     var $hamburger = $(".hamburger");
     $hamburger.on("click", function(e) {
@@ -9,22 +9,22 @@ jQuery(document).ready(function(){
     $("#redir-abt").click(function() {
         $('html, body').animate({
             scrollTop: $("#about").offset().top
-        }, 2000);
-        jQuery('#navhidden').toggle('hide');
+        }, 1000);
+        jQuery('#navhidden').fadeToggle('slow');
         $hamburger.toggleClass("is-active");
     });
     $("#redir-dev").click(function() {
         $('html, body').animate({
             scrollTop: $("#dev").offset().top
-        }, 2000);
-        jQuery('#navhidden').toggle('hide');
+        }, 1000);
+        jQuery('#navhidden').fadeToggle('slow');
         $hamburger.toggleClass("is-active");
     });
     $("#redir-img").click(function() {
         $('html, body').animate({
             scrollTop: $("#img").offset().top
-        }, 2000);
-        jQuery('#navhidden').toggle('hide');
+        }, 1000);
+        jQuery('#navhidden').fadeToggle('slow');
         $hamburger.toggleClass("is-active");
     });
 });
--- content/jq.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.redir-about').click(function() {
-    $('html, body').animate({
-      scrollTop: $("container.about").offset().top
-    }, 1000)
-}), 
