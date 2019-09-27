3729838f08d99cc7ece23ca0a8ff92722e6a3916
shagrirse <shagrirse@gmail.com>
2019-09-27 17:32:30 +0800

    i need to eat dinner with c00l kids

--- content/int.js
+++ content/int.js
@@ -2,10 +2,12 @@ jQuery(document).ready(function(){
     jQuery('#navbar').on('click', function(event) {        
         jQuery('#navhidden').fadeToggle('fast');
     });
+
     var $hamburger = $(".hamburger");
     $hamburger.on("click", function(e) {
     $hamburger.toggleClass("is-active");
     });
+
     $("#redir-abt").click(function() {
         $('html, body').animate({
             scrollTop: $("#about").offset().top
@@ -13,6 +15,7 @@ jQuery(document).ready(function(){
         jQuery('#navhidden').fadeToggle('slow');
         $hamburger.toggleClass("is-active");
     });
+
     $("#redir-dev").click(function() {
         $('html, body').animate({
             scrollTop: $("#dev").offset().top
@@ -20,6 +23,7 @@ jQuery(document).ready(function(){
         jQuery('#navhidden').fadeToggle('slow');
         $hamburger.toggleClass("is-active");
     });
+
     $("#redir-img").click(function() {
         $('html, body').animate({
             scrollTop: $("#img").offset().top
@@ -27,4 +31,13 @@ jQuery(document).ready(function(){
         jQuery('#navhidden').fadeToggle('slow');
         $hamburger.toggleClass("is-active");
     });
+
+    $(window).bind('scroll', function() {
+        if ($(window).scrollTop() > 1) {
+            $('#container1').hide('slow');
+        }
+        else {
+            $('#whole').css('display', 'block');
+        }
+   });
 });
